7ac92fa8a466f8d1f21112366213f6d9ab0403fd
Daria Vostroknutova <justdsarap@gmail.com>
2025-04-21 20:42:47 +0200

    feat: added header

--- src/widgets/header/header.component.ts
+++ src/widgets/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    template: `<h1>Header Component</h1>`,
-    styles: [],
-    standalone: true
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
+  standalone: true,
 })
 export class HeaderComponent {}
